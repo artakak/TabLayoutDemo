feb224fce9b8f721ca5bd156db0a4a51fa162479
artakak <a260641139@ya.ru>
2018-04-17 14:21:49 +0300

    sdf

--- app/src/main/java/com/example/art/tablayoutdemo/Tab1Fragment.kt
+++ app/src/main/java/com/example/art/tablayoutdemo/Tab1Fragment.kt
@@ -1,30 +1,32 @@
 package com.example.art.tablayoutdemo
 
 
+import android.app.Activity
 import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.preference.PreferenceManager
 import android.support.v4.app.Fragment
 import android.util.Log
+import android.util.Printer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
-import kotlinx.android.synthetic.main.fragment_tab4.*
 
 
 /**
  * A simple [Fragment] subclass.
  */
-class Tab4Fragment : Fragment() {
+class Tab1Fragment : Fragment() {
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_tab4, container, false)
-        val button3 = view.findViewById<Button>(R.id.button3)
-        button3.setOnClickListener {
+        val view = inflater.inflate(R.layout.fragment_tab1, container, false)
+        val button = view.findViewById<Button>(R.id.button)
+        button.setOnClickListener {
             Toast.makeText(activity, "Fragment", Toast.LENGTH_LONG).show()
             val string1 = "name"
             val string2 = "test1"
