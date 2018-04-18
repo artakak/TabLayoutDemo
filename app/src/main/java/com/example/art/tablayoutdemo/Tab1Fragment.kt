1e72e498b596006fba14e90d2917a9b1938c3fa5
artakak <a260641139@ya.ru>
2018-04-18 12:18:33 +0300

    sdf

--- app/src/main/java/com/example/art/tablayoutdemo/Tab1Fragment.kt
+++ app/src/main/java/com/example/art/tablayoutdemo/Tab1Fragment.kt
@@ -2,6 +2,7 @@ package com.example.art.tablayoutdemo
 
 
 import android.app.Activity
+import android.app.FragmentManager
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
@@ -12,6 +13,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 
@@ -25,7 +27,9 @@ class Tab1Fragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_tab1, container, false)
+        val textView = view.findViewById<TextView>(R.id.textView)
         val button = view.findViewById<Button>(R.id.button)
+        val button2 = view.findViewById<Button>(R.id.button2)
         button.setOnClickListener {
             Toast.makeText(activity, "Fragment", Toast.LENGTH_LONG).show()
             val string1 = "name"
@@ -36,6 +40,10 @@ class Tab1Fragment : Fragment() {
             editor.commit()
             val string3 = preferences.getString(string1, "None")
             Log.d("asd", string3)
+        }
+        button2.setOnClickListener{
+            textView.text = "BLABLA"
+            Log.d("asd", textView.text as String)
         }
         return view
     }
